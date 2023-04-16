2bd0fd5f4642ef18283089d7e803dfd9d38b14f7
anemology <anemology@users.noreply.github.com>
2023-04-16 16:44:08 +0800

    Refactor NewPage function

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 function NewPage() {
-    var list = document.getElementById("textarea").value.split("\n");
+    const list = document.getElementById("textarea").value.split("\n");
+    const useRegex = document.getElementById("useRegex").checked;
+    const imgarea = document.getElementById("imgarea");
+    const showURLs = document.getElementById("showURLs");
 
-    // remove all images
-    var imgarea = document.getElementById("imgarea");
-    while (imgarea.firstChild) {
-        imgarea.removeChild(imgarea.firstChild);
-    }
-    document.getElementById("showURLs").value = "";
+    // remove all images and clear showURLs
+    imgarea.innerHTML = "";
+    showURLs.value = "";
 
-    if (document.getElementById("useRegex").checked) {
-        var min = parseInt(document.getElementById("min").value);
-        var maxloop = parseInt(document.getElementById("max").value);
-        var step = parseInt(document.getElementById("step").value);
-        var isAddLeadingZero = document.getElementById("isAddLeadingZero").checked;
-        var numberOfWidth = parseInt(document.getElementById("numberWidth").value);
+    if (useRegex) {
+        const min = parseInt(document.getElementById("min").value);
+        const maxloop = parseInt(document.getElementById("max").value);
+        const step = parseInt(document.getElementById("step").value);
+        const isAddLeadingZero = document.getElementById("isAddLeadingZero").checked;
+        const numberOfWidth = parseInt(document.getElementById("numberWidth").value);
 
-        for (var j = min; j <= maxloop; j += step) {
-            var replaceNumberString = j + "";
+        for (let j = min; j <= maxloop; j += step) {
+            let replaceNumberString = j + "";
             if (isAddLeadingZero) {
                 replaceNumberString = addLeadingZeros(replaceNumberString, numberOfWidth);
             }
 
-            var oneUrl = list[0].replace("*", replaceNumberString);
-            var imgs = document.createElement("img");
+            const oneUrl = list[0].replace("*", replaceNumberString);
+            let imgs = document.createElement("img");
             imgs.src = oneUrl;
             imgs.alt = oneUrl;
             // add image url to textarea if loaded, then sort
             imgs.addEventListener("load", function () {
-                var text = document.getElementById("showURLs").value;
+                let text = showURLs.value;
                 if (text) text += "\n";
                 text += this.src;
-                document.getElementById("showURLs").value = text.split("\n").sort().join("\n");
+                showURLs.value = text.split("\n").sort().join("\n");
             });
             // remove element if image load failed
             imgs.addEventListener("error", function () {
@@ -39,8 +39,8 @@ function NewPage() {
             imgarea.appendChild(imgs);
         }
     } else {
-        for (var i = 0; i <= list.length - 1; i++) {
-            var imgs = document.createElement("img");
+        for (let i = 0; i < list.length; i++) {
+            let imgs = document.createElement("img");
             imgs.src = list[i];
             imgs.alt = list[i];
             // remove element if image load failed
